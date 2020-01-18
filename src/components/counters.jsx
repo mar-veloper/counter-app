e8c21ad48aa0be116b262d53bef6064e38156d83
mar-veloper <42553243+mar-veloper@users.noreply.github.com>
2020-01-18 14:55:44 +0100

    raising events

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,16 +5,22 @@ class Counters extends Component {
   state = {
     counters: [
       { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
+      { id: 2, value: 3 },
+      { id: 3, value: 3 },
+      { id: 4, value: 1 }
     ]
   };
 
   render() {
     return (
       <div>
-        <Counter></Counter>
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            value={counter.value}
+            id={counter.id}
+          ></Counter>
+        ))}
       </div>
     );
   }
